970451291c28cf66358e69f1373d4a4f3650d2f6
Tue Vu <vu.tue.1710@gmail.com>
2018-09-26 23:56:04 +0700

    feature[code-splitting]: add code splitting into project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from 'pages/Home'
 
 class App extends Component {
   render () {
-    return <div className='App' />
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default class ABC extends React.Component {
-  render () {
-    return <h1>adasf</h1>
-  }
-}
